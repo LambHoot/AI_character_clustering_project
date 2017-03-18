420ac5c96717be4081fe3baa13937eee79b161b1
Denis <deniskefallinos94@gmail.com>
2017-03-18 15:05:07 -0400

    normalization

--- AI_Project/AI_Project/ImageHelper.cs
+++ AI_Project/AI_Project/ImageHelper.cs
@@ -3,19 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace AI_Project
 {
     class ImageHelper
     {
-
-
-        public static void normalizeImage(string path)
+        public static Bitmap normalizeImage(string path)
         {
-
+            Bitmap myBitmap = new Bitmap(path);
+            return normalizeImageSize(normalizeImageColor(myBitmap));
         }
 
+        public static Bitmap normalizeImageColor(Bitmap myBitmap)
+        {
+            for (int i = 0; i < myBitmap.Height; i++)
+            {
+                for (int j = 0; j < myBitmap.Width; j++)
+                {
+                    if (myBitmap.GetPixel(i, j) != Color.White)
+                        myBitmap.SetPixel(i, j, Color.Black);
+                }
+            }
+            return myBitmap;
+        }
 
+        //from http://www.deltasblog.co.uk/code-snippets/c-resizing-a-bitmap-image/
+        public static Bitmap normalizeImageSize(Bitmap myBitmap)
+        {
+            Bitmap result = new Bitmap(42, 42);
+            using (Graphics g = Graphics.FromImage(result))
+                g.DrawImage(myBitmap, 0, 0, 42, 42);
+            return result;
+        }
 
 
     }
