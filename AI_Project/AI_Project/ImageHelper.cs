38dff6c8f14f3e5156319c5b59c3b0ed9ee615ab
Denis <deniskefallinos94@gmail.com>
2017-03-18 14:41:39 -0400

    aye hommes

---
+++ AI_Project/AI_Project/ImageHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_Project
+{
+    class ImageHelper
+    {
+
+
+        public static void normalizeImage(string path)
+        {
+
+        }
+
+
+
+
+    }
+}
