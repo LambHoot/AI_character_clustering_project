60f7d783b85c15fc854c519c659db86ba9c622e8
Denis <deniskefallinos94@gmail.com>
2017-03-22 16:00:18 -0400

    can't get enough of that sugar crisp!

---
+++ AI_Project/AI_Project/KNNwithBayes.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AI_Project;
+
+namespace AI_Project
+{
+    public class KNNwithBayes
+    {
+        public List<SectionedImage> trainingImages = new List<SectionedImage>(), testingImages = new List<SectionedImage>();
+        public List<ClassifiedTestingImage> ClassifiedImagePairs = new List<ClassifiedTestingImage>();
+        //trainingImages one for each character
+
+        public List<ClassifiedTestingImage> RunKNNwithBayes(List<string> trainingImagesPaths, List<string> testingImagesPaths)
+        {
+            return null;
+        }
+
+        public void ExtractFeatures(List<string> trainingImagesPaths, List<string> testingImagesPaths)
+        {
+            trainingImages = GradientHistogram.ExtractFeaturesForImages(trainingImagesPaths);
+            List<List<ImageBlockFeature>> trainingFeatures = new List<List<ImageBlockFeature>>();
+
+            testingImages = GradientHistogram.ExtractFeaturesForImages(testingImagesPaths);
+            List<List<ImageBlockFeature>> testingFeatures = new List<List<ImageBlockFeature>>();
+
+            //make list of training features
+            //organized in lists of lists of imageblocks with classes, based on block order
+            //ex) one list for all images top left block, one list for all images top right block, etc...
+            int blockNum = 0;
+            while (blockNum < 36) {
+                List<ImageBlockFeature> blockList = new List<ImageBlockFeature>();
+                foreach (SectionedImage si in trainingImages)
+                {
+                    blockList.Add(new ImageBlockFeature(si.blocks[blockNum], si.path.Split('\\')[5]));
+                }
+                blockNum++;
+                trainingFeatures.Add(blockList);
+            }
+
+
+        }
+
+        public int getProbOfClass(List<List<ImageBlockFeature>> features, string classDigit)
+        {
+            int countClass = 0;
+            int countTotal = 0;
+            foreach(List<ImageBlockFeature> lbf in features)
+            {
+                foreach (ImageBlockFeature ibf in lbf)
+                {
+                    if (ibf.classifiedDigit.Equals(classDigit))
+                        countClass++;
+                    countTotal++;
+                }
+            }
+            return countClass / countTotal;
+        }
+
+
+    }
+
+
+    public class ImageBlockFeature {
+        public ImageBlock block;
+        public string classifiedDigit;
+
+        public ImageBlockFeature(ImageBlock ib, string digit)
+        {
+            this.block = ib;
+            this.classifiedDigit = digit;
+        }
+
+    }
+
+
+
+
+}
---
+++ AI_Project/KNNwithBayes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AI_Project;
+
+namespace AI_Project
+{
+    public class KNNwithBayes
+    {
+        public List<SectionedImage> trainingImages = new List<SectionedImage>(), testingImages = new List<SectionedImage>();
+        public List<ClassifiedTestingImage> ClassifiedImagePairs = new List<ClassifiedTestingImage>();
+        //trainingImages one for each character
+
+        public List<ClassifiedTestingImage> RunKNN(List<string> trainingImagesPaths, List<string> testingImagesPaths)
+        {
+
+        }
+
+        public void ExtractFeatures(List<string> trainingImagesPaths, List<string> testingImagesPaths)
+        {
+            trainingImages = GradientHistogram.ExtractFeaturesForImages(trainingImagesPaths);
+            List<ImageBlockFeature> trainingFeatures = new List<ImageBlockFeature>();
+
+            testingImages = GradientHistogram.ExtractFeaturesForImages(testingImagesPaths);
+            List<ImageBlockFeature> testingFeatures = new List<ImageBlockFeature>();
+
+            //make list of training features
+            foreach(SectionedImage si in trainingImages)
+            {
+                foreach(ImageBlock)
+
+
+                trainingFeatures.Add(new ImageBlockFeature(si));
+
+
+                si.path;
+            }
+
+
+
+
+        }
+
+
+    }
+
+
+    public class ImageBlockFeature{
+        public ImageBlock block;
+        public string classifiedDigit;
+
+        ImageBlockFeature(ImageBlock ib, string digit)
+        {
+            this.block = ib;
+            this.classifiedDigit = digit;
+        }
+
+    }
+
+
+
+}
