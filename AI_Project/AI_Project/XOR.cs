27b0d6acbd012c65a91b2e1830a4c7a4b9c97d47
pmiri <pmiriello@gmail.com>
2017-03-29 14:08:23 -0400

    XOR class added

---
+++ AI_Project/AI_Project/XOR.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_Project
+{
+    /**
+     * NOTE: XOR IMAGES and test that way
+     * ALSO, IMPLEMENT STRATEGY PATTEN
+     */
+
+    class XOR
+    {
+    }
+}
