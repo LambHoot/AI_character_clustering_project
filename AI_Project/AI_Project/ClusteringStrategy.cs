1e30b1d8cf4824e649afce7cf409fed6c5ca0df1
Denis <deniskefallinos94@gmail.com>
2017-03-29 16:01:03 -0400

    yeeeeeeeeee boi

--- AI_Project/AI_Project/ClusteringStrategy.cs
+++ AI_Project/AI_Project/ClusteringStrategy.cs
@@ -10,4 +10,15 @@ namespace AI_Project
     {
         float getSimilarity(SectionedImage i1, SectionedImage i2);//lower score means more similar
     }
+
+
+    class EuclideanDistanceClustering : ClusteringStrategy
+    {
+
+        public float getSimilarity(SectionedImage i1, SectionedImage i2)
+        {
+            return (float)Math.Sqrt(Math.Pow(i1.x + i2.x, 2) + Math.Pow(i1.y + i2.y, 2));
+        }
+    }
+
 }
