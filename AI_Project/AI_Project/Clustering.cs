baceffa584a5fa7fc90b03bac41bb8254f498a42
Denis <deniskefallinos94@gmail.com>
2017-03-29 15:23:31 -0400

    Clustering algorithm done

---
+++ AI_Project/AI_Project/Clustering.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_Project
+{
+    class Clustering
+    {
+
+
+        public static void DoClustering(ClusteringStrategy cs, List<SectionedImage> sectionedImages, float minSimilarity)
+        {
+            List<Cluster> clusterList = new List<Cluster>();
+            foreach(SectionedImage image in sectionedImages)
+            {
+                int similarClusterIndex = -1;
+                float currentClusterSimilarity = cs.getSimilarity(image, clusterList[0].center) + 1;
+                for(int i = 0; i < clusterList.Count(); i++)
+                {
+                    float nextSimilarity = cs.getSimilarity(image, clusterList[i].center);
+                    if (nextSimilarity < currentClusterSimilarity && nextSimilarity <= minSimilarity)
+                    {
+                        currentClusterSimilarity = nextSimilarity;
+                        similarClusterIndex = i;
+                    }
+                }
+                //for each image
+                if(similarClusterIndex != -1)
+                {
+                    //if the image matched a cluster, add to it and update the cluster
+                    clusterList[similarClusterIndex].addImage(image, cs);
+                    //then see if any clusters can be merged
+                    for(int i = 0; i < clusterList.Count(); i++)
+                    {
+                        for (int j = i+1; j < clusterList.Count(); j++)
+                        {
+                            if(cs.getSimilarity(clusterList[i].center, clusterList[j].center) <= minSimilarity)
+                            {
+                                clusterList.Add(Cluster.mergeClusters(clusterList[i], clusterList[j], cs));
+                                clusterList.RemoveAt(j);
+                                clusterList.RemoveAt(i);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Cluster newImageCluster = new Cluster(image);
+                    clusterList.Add(newImageCluster);
+                }
+            }
+            //all images processed and added to clusters at this point
+
+            var x = clusterList;
+
+            //pass something to draw it as a graph, gg done
+
+        }
+
+
+
+        public class Cluster
+        {
+            public SectionedImage center;
+            public List<SectionedImage> images;
+
+            public Cluster(SectionedImage center)
+            {
+                this.center = center;
+                images = new List<SectionedImage>();
+                images.Add(center);
+            }
+
+            public void addImage(SectionedImage si, ClusteringStrategy cs)
+            {
+                images.Add(si);
+                UpdateCenter(cs);
+            }
+
+            public void UpdateCenter(ClusteringStrategy cs)
+            {
+                float currentCenterSimilarity = 0;
+                foreach(SectionedImage image in images)
+                {
+                    currentCenterSimilarity += cs.getSimilarity(center, image);
+                }
+
+                for(int i = 0; i < images.Count(); i++)
+                {
+                    float iImageSimilarity = 0;
+                    for(int j = 0; j < images.Count(); j++)
+                    {
+                        iImageSimilarity += cs.getSimilarity(images[i], images[j]);
+                    }
+                    if(iImageSimilarity < currentCenterSimilarity)
+                    {
+                        center = images[i];
+                        UpdateCenter(cs);
+                    }
+                }
+            }
+
+            public static Cluster mergeClusters(Cluster c1, Cluster c2, ClusteringStrategy cs)
+            {
+                Cluster returnedCluster = new Cluster(c1.images[0]);
+                for (int i = 1; i < c1.images.Count(); i++)
+                    returnedCluster.images.Add(c1.images[i]);
+                for (int i = 0; i < c2.images.Count(); i++)
+                    returnedCluster.images.Add(c2.images[i]);
+                returnedCluster.UpdateCenter(cs);
+                return returnedCluster;
+            }
+
+
+        }
+
+
+
+
+    }
+}
---
+++ AI_Project/AI_Project/ClusteringStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_Project
+{
+    interface ClusteringStrategy
+    {
+        float getSimilarity(SectionedImage i1, SectionedImage i2);//lower score means more similar
+    }
+}
